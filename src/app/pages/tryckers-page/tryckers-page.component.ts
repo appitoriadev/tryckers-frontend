01fd2882e832d55fea6108781989c4db69e917a5
JudLup Luna <judlup@gmail.com>
2025-07-16 21:56:38 -0500

    feat: coimponente login  parte 1

--- src/app/pages/tryckers-page/tryckers-page.component.ts
+++ src/app/pages/tryckers-page/tryckers-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { HeaderComponent } from '../../components/header/header.component';
-import Login from '../auth/login/login';
 
 @Component({
   selector: 'app-tryckers-page',
-  imports: [HeaderComponent, FooterComponent, Login],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './tryckers-page.component.html',
 })
 export default class TryckersPageComponent {}
