979415f9c1f50b18ef114bc2231ea954d0727219
JudLup Luna <judlup@gmail.com>
2025-07-28 23:04:26 -0500

    merge y refactorización salvaje

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,27 @@
-
 import { Routes } from '@angular/router';
 import { NotAuthenticatedGuard } from '@auth/guards/not-authenticated.guard';
+import { MainLayoutComponent } from './shared/layouts/main-layout.component';
 
 export const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.routes'),
-     canMatch:[
-       NotAuthenticatedGuard
-    ]
+    canMatch: [NotAuthenticatedGuard],
   },
   {
     path: '',
-    loadChildren: () => import('./tryckers/tryckers.routes')
-  }
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./tryckers/pages/tryckers-page/tryckers-page.component'),
+      },
+    ],
+  },
 ];
